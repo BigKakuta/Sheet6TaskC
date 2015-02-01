2da8707dd33bd8e3cc6de8d238db6cdf8d926a76
BigKakuta <danielmeyer.dm.n@gmail.com>
2015-02-01 20:18:00 +0100

    new structur added packages

---
+++ src/edu/kit/informatik/Program.java
@@ -0,0 +1,9 @@
+package edu.kit.informatik;
+
+/**
+ * @author Daniel Meyer
+ * @version 1.0
+ */
+public class Program {
+
+}
--- src/src/Program.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by danielmeyer on 01/02/15.
- */
-public class Program {
-    public static void main(String args[]) {
-
-    }
-}
